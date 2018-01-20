c5f756bd25301e73a32afcebbb562fb976b05605
Lukas Heiland <st151210@stud.uni-stuttgart.de>
2018-01-20 16:59:30 +0100

    update

--- src/skp_6/MatrixMultiplication.java
+++ src/skp_6/MatrixMultiplication.java
@@ -1,5 +1,11 @@
 package skp_6;
 
+/**
+ * 
+ * @author Lukas Heiland
+ *
+ */
+
 public class MatrixMultiplication {
 
 	private static final String WRONG_FORMAT = "The format of the matrices doesn't allow multiplication!";
@@ -15,98 +21,148 @@ public class MatrixMultiplication {
 
 		// Ergebnismatrix hat soviele Zeilen wie A und so viele Spalten wie B
 		result = new int[matrixA.length][matrixB[0].length];
-		int[] matrixBColumn1 = { matrixB[0][0], matrixB[1][0], matrixB[2][0] };
-		int[] matrixBColumn2 = { matrixB[0][1], matrixB[1][1], matrixB[2][1] };
-		int[] matrixBColumn3 = { matrixB[0][2], matrixB[1][2], matrixB[2][2] };
-
-		for (int i = 0; i < matrixA.length; i++) {
-			SynchronousChannel channel = actualCalculationProcess(matrixA[i],i, matrixBColumn1, matrixBColumn2,
-					matrixBColumn3);
-//			int j = 0;
-//			try {
-//				while (channel.receive() != null) {
-//					try {
-//						result[i][j] = channel.receive();
-//					} catch (InterruptedException e) {
-//						System.out.println(ERROR_MESSAGE);
-//						e.printStackTrace();
-//					}
-//					j++;
-//				}
-//			} catch (InterruptedException e) {
-//				System.out.println(ERROR_MESSAGE);
-//				e.printStackTrace();
-//			}
+		
+		SynchronousChannel channel1 = new SynchronousChannel();
+		SynchronousChannel channel2 = new SynchronousChannel();
+		
+		SourceThread source = new MatrixMultiplication.SourceThread(channel2, matrixB[0][0]);
+		IntermediateThread intermediate = new MatrixMultiplication.IntermediateThread(channel1, channel2, matrixA[0][0]);
+		ResultConsumerThread result = new MatrixMultiplication.ResultConsumerThread(channel1);
+		source.run();
+		intermediate.run();
+		result.run();
+		
+		try {
+			source.join();
+			intermediate.join();
+			result.join();
+		} catch (InterruptedException e) {
+			System.out.println(ERROR_MESSAGE);
+			e.printStackTrace();
 		}
-
-		// this is what the multiplication with loops would look like:
-		// for (int i = 0; i < result.length; i++) {
-		// for (int j = 0; j < result[i].length; j++) {
-		// System.out.print(result[i][j] + " ");
-		// }
-		// System.out.println("\n");
-		// }
+		
 
 		return MatrixMultiplication.result;
 	}
-
-	private static SynchronousChannel actualCalculationProcess(int[] matrixARow, int numberOfRow, int[]... matrixBColumns) {
-		SynchronousChannel channel = new SynchronousChannel();
-		Thread[] threads = new Thread[matrixBColumns.length];
-
-		for (int column = 0; column < matrixBColumns.length; column++) {
-			final int column1 = column;
-
-			/*
-			 * Create a thread which multiplies one row of matrix A with the
-			 * columns of matrix B.
-			 */
-			threads[column] = new Thread(new Runnable() {
-
-				@Override
-				public void run() {
-					try {
-							channel.send(multiply(matrixARow, matrixBColumns[column1]));
-							result[numberOfRow][column1] = channel.receive();
-						
-					} catch (InterruptedException e) {
-						System.out.println(ERROR_MESSAGE);
-						e.printStackTrace();
-					}
-				}
-
-				/**
-				 * Multiplies one row of a <code>matrix A</code> with one
-				 * <code>column</code> of a matrix B, thus producing one entry
-				 * of the resulting matrix (<code>result</code>).
-				 * 
-				 * @param matrixARow
-				 * @param column
-				 * @return result
-				 */
-				private Integer multiply(int[] matrixARow, int[] column) {
-					int result = 0;
-					for (int i = 0; i < matrixARow.length; i++) {
-						result += matrixARow[i] * column[i];
-					}
-					return result;
-				}
-			});
-
-			// run the Thread
-			threads[column].run();
+	
+	//main method for testing
+	public static void main(String[] args) {
+		int[][] A = {{1}}, B = {{2}};
+		
+		System.out.println(MatrixMultiplication.multiplyMatrices(A, B));
+	}
+	
+	/**
+	 * private class which first receives then sends a value.
+	 * 
+	 * @author Lukas Heiland
+	 *
+	 */
+	private static class IntermediateThread extends Thread {
+		public SynchronousChannel ownChannel;
+		private SynchronousChannel channelToReceiveFrom;
+		private Integer value;
+		
+		
+		
+		public IntermediateThread(SynchronousChannel ownChannel, SynchronousChannel channelToReceiveFrom, Integer value) {
+			super();
+			this.ownChannel = ownChannel;
+			this.channelToReceiveFrom = channelToReceiveFrom;
+			this.value = value;
 		}
 
-		// close the calculation threads
-		for (int i = 0; i < threads.length; i++) {
+
+		@Override
+		public void run() {
+			super.run();
+			System.out.println(this.getName() + " of type " + this.getClass().getName() + " started");
+			
 			try {
-				threads[i].join();
+				Integer received = channelToReceiveFrom.receive();
+				ownChannel.send(received + value);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} finally {
+				System.out.println("Execution of  Thread " + this.getName() + " finished");
+			}
+		}
+	}
+	
+	
+	
+	/**
+	 * Thread in which a channel only sends and then waits for a receive()-call.
+	 * 
+	 * @author Lukas Heiland
+	 */
+	private static class SourceThread extends Thread {
+		public SynchronousChannel ownChannel;
+		private Integer value;
+		
+		public SourceThread(SynchronousChannel ownChannel, Integer value) {
+			super();
+			this.ownChannel = ownChannel;
+			this.value = value;
+		}
+
+		@Override
+		public void run() {
+			super.run();
+			System.out.println(this.getName() + " of type " + this.getClass().getName() + " started");
+			
+			try {
+				ownChannel.send(value);
+			} catch (InterruptedException e) {
+				System.out.println(ERROR_MESSAGE);
+				e.printStackTrace();
+			} finally {
+				System.out.println("Execution of  Thread " + this.getName() + " finished");
+			}
+		}
+	}
+	
+	
+	
+	/**
+	 * Thread in which the channel only receives a value from a <code>ThreadA</code>
+	 * 
+	 * @author Lukas Heiland
+	 */
+	private static class ResultConsumerThread extends Thread {
+		public SynchronousChannel channelToReceiveFrom;
+		
+		public ResultConsumerThread(SynchronousChannel channelToReceiveFrom){
+			super();
+			this.channelToReceiveFrom = channelToReceiveFrom;
+		}
+		
+		@Override
+		public void run() {
+			super.run();
+			System.out.println(this.getName() + " of type " + this.getClass().getName() + " started");
+			
+			try {
+				Integer result = channelToReceiveFrom.receive();
+				MatrixMultiplication.result[0][0] = result;
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-
-		return channel;
 	}
 
 }
+
+	
+
+
+
+
+	/* this is what the multiplication with loops would look like:
+		* for (int i = 0; i < result.length; i++) {
+		* 	for (int j = 0; j < result[i].length; j++) {
+		* 		System.out.print(result[i][j] + " ");
+		* 	}
+		* 	System.out.println("\n");
+		* }
+	*/
